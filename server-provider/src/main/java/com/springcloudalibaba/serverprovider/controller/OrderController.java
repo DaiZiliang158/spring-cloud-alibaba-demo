f66e985621d9ba7278ae2acae0152e6d63a0a066
DaiZiliang158 <690862265@qq.com>
2019-11-07 19:07:48 +0800

    实现了sentinel的练习 但是后台没有显示出sentinel控制台要设置的选项

--- server-provider/src/main/java/com/springcloudalibaba/serverprovider/controller/OrderController.java
+++ server-provider/src/main/java/com/springcloudalibaba/serverprovider/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.springcloudalibaba.serverprovider.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class OrderController {
 
-
+    @Value("${server.port}")
+    private int port ;
     @RequestMapping("/orders/{uid}")
     public String list(@PathVariable int uid) {
         log.info(uid + "");
-        return "订单服务" ;
+        return port + "订单服务" ;
     }
 }
--- server-sentinel/src/main/java/com/springcloudalibaba/serversentinel/ServerSentinelApplication.java
+++ server-sentinel/src/main/java/com/springcloudalibaba/serversentinel/ServerSentinelApplication.java
@@ -2,8 +2,12 @@ package com.springcloudalibaba.serversentinel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class ServerSentinelApplication {
 
     public static void main(String[] args) {
